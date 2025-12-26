571149364ba93f8e4aa6c796d5a32c7015c638fa
hminh <hminh250104@gmail.com>
2025-12-27 03:04:11 +0700

    feat: Set default error message for DomainException

--- app/src/main/java/com/example/simpleweather/domain/exception/DomainException.kt
+++ app/src/main/java/com/example/simpleweather/domain/exception/DomainException.kt
@@ -1,9 +1,10 @@
 package com.example.simpleweather.domain.exception
 
 import androidx.annotation.StringRes
+import com.example.simpleweather.R
 
 sealed class DomainException(
     message: String? = null,
     cause: Throwable? = null,
-    @StringRes val messageResId: Int? = null
+    @StringRes val messageResId: Int? = R.string.general_error
 ) : Exception(message, cause)
