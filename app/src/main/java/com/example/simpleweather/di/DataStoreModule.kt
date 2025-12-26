65699da08953c34b7ca948e39b66860006bba0da
hminh <hminh250104@gmail.com>
2025-12-27 02:58:29 +0700

    refactor: Simplify dependency injection for UseCases and DataStore

--- app/src/main/java/com/example/simpleweather/di/DataStoreModule.kt
+++ app/src/main/java/com/example/simpleweather/di/DataStoreModule.kt
@@ -1,11 +1,11 @@
 package com.example.simpleweather.di
 
 import android.content.Context
-import com.example.simpleweather.domain.repository.UserSettingRepository
-import com.example.simpleweather.data.source.local.UserSettingRepositoryImpl
+import com.example.simpleweather.data.source.local.UserSettingDataSource
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import jakarta.inject.Singleton
 
@@ -15,8 +15,8 @@ object DataStoreModule {
     @Singleton
     @Provides
     fun provideSettingDataStore(
-        context: Context
-    ): UserSettingRepository {
-        return UserSettingRepositoryImpl(context)
+        @ApplicationContext context: Context
+    ): UserSettingDataSource {
+        return UserSettingDataSource(context)
     }
 }
--- app/src/main/java/com/example/simpleweather/di/UseCaseModule.kt
+++ app/src/main/java/com/example/simpleweather/di/UseCaseModule.kt
@@ -1,33 +1,23 @@
 package com.example.simpleweather.di
 
-import android.content.Context
-import com.example.simpleweather.data.source.local.UserSettingRepositoryImpl
 import com.example.simpleweather.domain.repository.UserSettingRepository
 import com.example.simpleweather.domain.usecase.GetUserSettingUseCase
 import com.example.simpleweather.domain.usecase.SetUserSettingUseCase
-import com.example.simpleweather.domain.usecase.UseCase
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
-import jakarta.inject.Named
-import jakarta.inject.Singleton
-import kotlin.jvm.JvmSuppressWildcards
 
 @Module
 @InstallIn(SingletonComponent::class)
 object UseCaseModule {
     @Provides
-    @Named("getUserSetting")
-    @JvmSuppressWildcards
     fun provideGetUserSettingUseCase(
         userSettingRepository: UserSettingRepository
-    ): UseCase<GetUserSettingUseCase.Params, GetUserSettingUseCase.Output> = GetUserSettingUseCase(userSettingRepository)
+    ): GetUserSettingUseCase = GetUserSettingUseCase(userSettingRepository)
 
     @Provides
-    @Named("setUserSetting")
-    @JvmSuppressWildcards
     fun provideSetUserSettingUseCase(
         userSettingRepository: UserSettingRepository
-    ): UseCase<SetUserSettingUseCase.Params, SetUserSettingUseCase.Output> = SetUserSettingUseCase(userSettingRepository)
+    ): SetUserSettingUseCase = SetUserSettingUseCase(userSettingRepository)
 }
